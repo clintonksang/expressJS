f310be484019fb2f97c16aa0db0752a0e4aae79a
fixset <clintonksang@gmail.com>
2020-07-24 23:55:38 +0300

    init

---
+++ firstapp/firstapp.js
@@ -0,0 +1,6 @@
+// import module from './module'; resebles below
+
+// const logger = require('./module');
+import log from  './module.js';
+
+console.log(5)
---
+++ firstapp/module.js
@@ -0,0 +1,11 @@
+// export function log(
+//     message   //parameters
+//     ){
+//      console.log(message); //Function body     //function body
+// }
+
+//   //logger is a named param wile log is a reference to te param above
+
+export function log(n){
+ return n * 2;
+}
